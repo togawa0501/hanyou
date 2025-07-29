9e07cfbfb94f045c5b7aeb8498d15e7661c268c2
togawa0501 <8214tgwcsn@gmail.com>
2025-07-29 16:28:14 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// ▼▼▼ この部分を、あなたのFirebaseプロジェクトの設定情報に書き換えてください ▼▼▼
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_fto_GZSLtYhOwU9Zma0GkRv_MjPcBtk",
+  authDomain: "hanyou-system.firebaseapp.com",
+  projectId: "hanyou-system",
+  storageBucket: "hanyou-system.firebasestorage.app",
+  messagingSenderId: "354073174422",
+  appId: "1:354073174422:web:283c7774ae83ac62a8fdb7"
+};
+
+// ▲▲▲ ここまで ▲▲▲
+
+
+// Firebaseを初期化
+firebase.initializeApp(firebaseConfig);
